803b34395000bb220f47ca4b4d027061c4a6df8f
mahamadi congo <mahamadicongo@hotmail.com>
2020-05-01 01:06:35 +0200

    hi

--- middleware/security.js
+++ middleware/security.js
@@ -1,10 +1,12 @@
 exports.setCors = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, x-Requested-With, Content-Type, Accept'
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
   );
-  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 };
