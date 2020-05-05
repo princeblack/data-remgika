0c1394f0347fd4d831f0b2fce32c631e4218b0c9
princeblack <mahamadicongo@hotmail.com>
2020-05-05 19:08:08 +0200

    cors origin

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://remgika.com",
+    origin: "*",
     credentials: true,
   })
 );
